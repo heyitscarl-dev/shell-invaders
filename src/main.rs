4cd7f81f6d47f4601b141202979b9eb520aa6457
heyitscarl <about@heyitscarl.dev>
2025-08-07 11:18:29 +0200

    wip: bullets

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::{io::{stdout, Write}, process::exit, thread::sleep, time::Duration};
+use std::{io::{stdout, Write}, thread::sleep, time::{Duration, Instant}};
 
-use crossterm::{cursor::{self, MoveTo}, event::{self, Event, KeyCode}, execute, style, terminal::{self, Clear, ClearType}, QueueableCommand};
-use hecs::{With, World};
+use crossterm::{cursor, event::{self, Event, KeyCode, KeyEventKind}, execute, style, terminal::{self, ClearType}};
+use hecs::World;
 
 struct Position {
-    x: u16,
-    y: u16
+    x: i16,
+    y: i16,
+}
+
+struct Velocity {
+    x: i16,
+    y: i16,
 }
 
 struct Renderable {
-    glyph: char
+    glyph: char,
 }
 
 fn main() -> std::io::Result<()> {
@@ -20,38 +25,76 @@ fn main() -> std::io::Result<()> {
 
     let mut world = World::new();
 
-    world.spawn((
-        Position { x: 10, y: 10 },
-        Renderable { glyph: '@' }
+    let player = world.spawn((
+        Position { x: 10, y: 100 },
+        Renderable { glyph: '@' },
     ));
 
     'game: loop {
         // Handle input 
         while event::poll(Duration::from_millis(0))? {
             if let Event::Key(key) = event::read()? {
-                for (_, pos) in world.query_mut::<&mut Position>() {
-                    match key.code {
-                        KeyCode::Left => {
-                            if pos.x > 0 {
-                                pos.x -= 1;
-                            }
-                        },
-                        KeyCode::Right => {
-                            pos.x += 1;
-                        },
-                        KeyCode::Char('q') => break 'game,
+                // Game quitting
+                if let KeyCode::Char('q') = key.code {
+                    break 'game;
+                }
+
+                // Player controlling
+                if let Ok(vel) = world.query_one_mut::<&mut Velocity>(player) {
+                    match (key.kind, key.code) {
+                        ( KeyEventKind::Press | KeyEventKind::Repeat, KeyCode::Left ) => vel.x = 1,
+                        ( KeyEventKind::Press | KeyEventKind::Repeat, KeyCode::Right ) => vel.x = -1,
+                        ( KeyEventKind::Release, KeyCode::Left | KeyCode::Right ) => vel.x = 0,
                         _ => {}
                     }
                 }
+
+                // Bullet spawning
+                let bullet = if let Ok(pos) = world.get::<&Position>(player) {
+                    match key.code {
+                        KeyCode::Char(' ') => {
+                            Some((
+                                Position { x: pos.x, y: pos.y },
+                                Velocity { x: 0, y: -1 },
+                                Renderable { glyph: '*' }
+                            ))
+                        },
+                        _ => None
+                    }
+                } else { None };
+
+                if let Some(bullet) = bullet {
+                    world.spawn(bullet);
+                }
+            }
+        }
+
+        // Entity movement
+        for (_, (pos, vel)) in world.query_mut::<(&mut Position, &mut Velocity)>() {
+            let new_x = pos.x + vel.x;
+            let new_y = pos.y + vel.y;
+
+            if new_x >= 0 {
+                pos.x = new_x;
+            }
+
+            if new_y >= 0 {
+                pos.y = new_y;
             }
         }
 
         // Render entities 
         for (_, (pos, render)) in world.query::<( &Position, &Renderable )>().iter() {
-            execute!(stdout, terminal::Clear(ClearType::All), cursor::MoveTo(pos.x, pos.y), style::Print(render.glyph))?;
+            execute!(
+                stdout,
+                terminal::Clear(ClearType::All),
+                cursor::MoveTo(pos.x as u16, pos.y as u16), 
+                style::Print(render.glyph)
+            )?;
         }
 
         stdout.flush()?;
+        sleep(Duration::from_millis(1000 / 60));
     }
 
     // Cleanup
