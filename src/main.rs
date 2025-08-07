bb59c9abb908684229424d3c60c0acdd600b87b0
heyitscarl <about@heyitscarl.dev>
2025-08-07 18:18:11 +0200

    wip: bullets & colliders

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::{io::{stdout, Write}, thread::sleep, time::{Duration, Instant}};
+use std::{io::{stdout, Write}, thread::sleep, time::Duration};
 
-use crossterm::{cursor, event::{self, Event, KeyCode, KeyEventKind}, execute, style, terminal::{self, ClearType}};
+use crossterm::{cursor, event::{self, Event, KeyCode, KeyEventKind}, execute, style, terminal::{self, size, Clear, ClearType}};
 use hecs::World;
 
 struct Position {
+    x: u16,
+    y: u16,
+}
+
+struct ScreenPosition {
     x: i16,
-    y: i16,
+    y: i16
 }
 
 struct Velocity {
@@ -14,10 +19,17 @@ struct Velocity {
 }
 
 struct Renderable {
-    glyph: char,
+    data: Vec<&'static str>,
+}
+
+struct Collider {
+    width: u16,
+    height: u16
 }
 
 fn main() -> std::io::Result<()> {
+    let (width, height) = size().unwrap();
+
     // Preparation
     let mut stdout = stdout();
     terminal::enable_raw_mode()?;
@@ -26,13 +38,31 @@ fn main() -> std::io::Result<()> {
     let mut world = World::new();
 
     let player = world.spawn((
-        Position { x: 10, y: 100 },
-        Renderable { glyph: '@' },
+        Position { x: 10, y: 25 },
+        Velocity { x: 0, y: 0 },
+        Renderable { data: vec!["^"] },
     ));
 
+    world.spawn((
+        ScreenPosition { x: 2, y: 2 },
+        Renderable { data: vec!["Entities: $1", "FPS: $2", "TPS: $3"] }
+    ));
+
+    for x in 0 .. 5 {
+        for y in 0 .. 2 {
+            world.spawn((
+                Position { x: x * 9, y: y * 5},
+                Renderable { data: vec!["***", "***", " * "] },
+                Collider { width: 3, height: 3 }
+            ));
+        }
+    }
+
     'game: loop {
+        let mut bullet = None;
+
         // Handle input 
-        while event::poll(Duration::from_millis(0))? {
+        while event::poll(Duration::from_millis(1))? {
             if let Event::Key(key) = event::read()? {
                 // Game quitting
                 if let KeyCode::Char('q') = key.code {
@@ -42,55 +72,105 @@ fn main() -> std::io::Result<()> {
                 // Player controlling
                 if let Ok(vel) = world.query_one_mut::<&mut Velocity>(player) {
                     match (key.kind, key.code) {
-                        ( KeyEventKind::Press | KeyEventKind::Repeat, KeyCode::Left ) => vel.x = 1,
-                        ( KeyEventKind::Press | KeyEventKind::Repeat, KeyCode::Right ) => vel.x = -1,
-                        ( KeyEventKind::Release, KeyCode::Left | KeyCode::Right ) => vel.x = 0,
-                        _ => {}
+                        ( KeyEventKind::Press | KeyEventKind::Repeat, KeyCode::Char('a') ) => vel.x = -1,
+                        ( KeyEventKind::Press | KeyEventKind::Repeat, KeyCode::Char('d') ) => vel.x = 1,
+                        _ => {
+                            vel.x = 0
+                        }
                     }
                 }
 
                 // Bullet spawning
-                let bullet = if let Ok(pos) = world.get::<&Position>(player) {
+                bullet = if let Ok(pos) = world.get::<&Position>(player) {
                     match key.code {
-                        KeyCode::Char(' ') => {
+                        KeyCode::Char('w') => {
                             Some((
                                 Position { x: pos.x, y: pos.y },
                                 Velocity { x: 0, y: -1 },
-                                Renderable { glyph: '*' }
+                                Collider { width: 1, height: 1 },
+                                Renderable { data: vec!["|"] }
                             ))
                         },
                         _ => None
                     }
                 } else { None };
+            }
+        }
 
-                if let Some(bullet) = bullet {
-                    world.spawn(bullet);
+        if let Some(bullet) = bullet {
+            world.spawn(bullet);
+        }
+
+        let mut to_despawn = Vec::new();
+            
+        // Entity movement
+        for (entity, (pos, vel)) in world.query_mut::<(&mut Position, &mut Velocity)>() {
+            let new_x = (pos.x as i16) + vel.x;
+            let new_y = (pos.y as i16) + vel.y;
+
+            if new_x >= 0 && (new_x as u16) < (width as u16) {
+                pos.x = new_x as u16;
+            }
+
+            if new_y >= 0 && (new_y as u16) < (height as u16) {
+                pos.y = new_y as u16;
+            } else {
+                to_despawn.push(entity);
+            }
+        }
+
+        for (inner, (pos_1, col_1)) in world.query::<(&Position, &Collider)>().iter() {
+            for (outer, (pos_2, col_2)) in world.query::<(&Position, &Collider)>().iter() {
+                if inner == outer {
+                    continue;
+                }
+
+                if pos_1.x >= pos_2.x && pos_1.x + col_1.width <= pos_2.x + col_2.width {
+                    if pos_1.y >= pos_2.y && pos_1.y + col_1.height <= pos_2.y + col_2.height {
+                        to_despawn.push(inner);
+                        to_despawn.push(outer);
+                    }
                 }
             }
         }
 
-        // Entity movement
-        for (_, (pos, vel)) in world.query_mut::<(&mut Position, &mut Velocity)>() {
-            let new_x = pos.x + vel.x;
-            let new_y = pos.y + vel.y;
-
-            if new_x >= 0 {
-                pos.x = new_x;
-            }
-
-            if new_y >= 0 {
-                pos.y = new_y;
-            }
+        // Despawn entities
+        for entity in to_despawn {
+            let _ = world.despawn(entity);
         }
 
         // Render entities 
+        execute!(stdout, Clear(ClearType::All))?;
         for (_, (pos, render)) in world.query::<( &Position, &Renderable )>().iter() {
-            execute!(
-                stdout,
-                terminal::Clear(ClearType::All),
-                cursor::MoveTo(pos.x as u16, pos.y as u16), 
-                style::Print(render.glyph)
-            )?;
+            for ( row_index, row_content ) in render.data.iter().enumerate() {
+                let x = pos.x;
+                let y = pos.y + row_index as u16;
+
+                execute!(
+                    stdout,
+                    cursor::MoveTo(x as u16, y as u16), 
+                    style::Print(row_content)
+                )?;
+            }
+        }
+
+        // Render UI
+        let count = world.len();
+
+        for (_, (pos, render)) in world.query::<( &ScreenPosition, &Renderable )>().iter() {
+            for ( row_index, row_content ) in render.data.iter().enumerate() {
+                let x = pos.x;
+                let y = pos.y + ( row_index as i16 );
+
+                let row_content = row_content
+                    .replace("$1", &format!("{}", count));
+
+                execute!(
+                    stdout,
+                    cursor::MoveTo(x as u16, y as u16), 
+                    style::Print(row_content)
+                )?;
+            }
         }
 
         stdout.flush()?;
